701f3308dd2e83c439165d4e92df312c8b880566
Chad Tindel <ctindel@Chads-MacBook-Pro-2.local>
2015-05-21 03:14:42 -0400

    fixed

--- api/v1.0/config/security.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    stormpath_secret_key : 'GBdEhS5PAPgo9RoDP8uEGvgxPUMFB8NbrWQJ6zL7',
-    stormpath_app_url : 'https://api.stormpath.com/v1/applications/4wDC5hbVrqvQRfJS5GMGru'
-}
