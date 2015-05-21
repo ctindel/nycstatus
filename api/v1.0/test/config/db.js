af6d919774676a29b5c1366f0ac48ab92006aec9
Chad Tindel <ctindel@Chads-MacBook-Pro-2.local>
2015-05-21 03:14:00 -0400

    fixed

--- api/v1.0/test/config/db.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    testDBURL : 'mongodb://localhost/nycstatus_test'
-}
--- api/v1.0/test/config/test_config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    url : 'http://localhost:8000/api/v1.0',
-    SP_APP_NAME : 'NYCStatus Test'
-}
